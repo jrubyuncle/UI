d44923c791c26135f9c52b93fe964daa5b879f37
wesleychu <jrubyuncle@gmail.com>
2015-12-11 16:07:06 +0800

    use tutor_path

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -5,9 +5,10 @@ class TutorsController < ApplicationController
 	#get 'chap10', 'chap10','chap11','chap12','chap13','chap14','chap15', controller: :tutors
 	#get 'chap20','chap21','chap22','chap23', controller: :tutors
 	#get 'chap30','chap31','chap32','chap33','chap34', controller: :tutors
-	
+
 	def show
-		render 'chap00'
+		render "chap0#{params[:id]}"
+		#render 'chap00'
 		# params[:id]
 	end
 
